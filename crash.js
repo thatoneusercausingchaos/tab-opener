fbe0bb36820e4b4c750b2293da9b3e58648c8c76
thatoneusercausingchaos <164237891+thatoneusercausingchaos@users.noreply.github.com>
2024-05-03 10:57:10 -0700

    Update crash.js

--- crash.js
+++ crash.js
@@ -1,11 +1,13 @@
+function crash() {
 	if (confirm('This will crash your browser!')) {
 	// Open Tabs
-	while (true){window.open("https://example.org")}
+	while (true){window.open('https://example.org')}
 	console.log('Starting Script...');
 } else {
 	//Do nothing
 	alert('Script Canceled!');
-	window.open("index.html");
+	window.location.href = 'index.html';
 	console.log('User Canceled...');
 	console.log('Nothing will happen');
+}
 }
